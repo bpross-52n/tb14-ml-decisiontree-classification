752edf03379eb5dcf310b5ef64517685572dc902
Benjamin Pross <b.pross@52north.org>
2018-06-14 12:19:48 +0200

    Add main

---
+++ src/main/scala/org/n52/project/testbed14/Main.scala
@@ -0,0 +1,13 @@
+package org.n52.project.testbed14
+
+object Main {
+  
+  def main(args: Array[String]) {
+    
+    val decisiontreeClassification = new DecisiontreeClassification
+    
+    decisiontreeClassification.run(args);
+    
+  }
+  
+}
